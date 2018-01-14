5fe05d4c0b9ee282a61e38368f706eae8f2cbef5
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 19:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const new_driver = Object.assign({}, driver, key, value)
+  const new_driver = Object.assign({}, driver, key: value)
 }
